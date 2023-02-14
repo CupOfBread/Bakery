debb754d3161c290a8a91af1bccd04a2fba19d7d
Toby <508087823@qq.com>
2023-02-14 15:17:00 +0800

    feat: 更新依赖

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -21,7 +21,7 @@ function clickToPage(path: string) {
 <template>
   <div class="text-center pt-8">
     <img
-      src="https://tva1.sinaimg.cn/large/005I8CXily1h6789ciuddj3098098q3f.jpg"
+      src="https://camo.githubusercontent.com/7d3a030b097c7aa9396f5bb0f1684a6ecb7527ce25708691fcfda52697a4b68e/68747470733a2f2f747661312e73696e61696d672e636e2f6c617267652f30303549384358696c7931683637383963697564646a333039383039387133662e6a7067"
       alt="Bakery"
       class="mx-auto"
     />
